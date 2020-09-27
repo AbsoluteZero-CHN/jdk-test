5091b2bfb25fa718aa48435f5165d81dc784dc2b
hao.caosh <hao.caosh@ttpai.cn>
2020-09-27 16:52:42 +0800

    初始提交

---
+++ src/cn/noload/chapter_1/Test.java
@@ -0,0 +1,11 @@
+package cn.noload.chapter_1;
+
+/**
+ * @author hao.caosh@ttpai.cn
+ * @date 2020-09-27 16:49
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
